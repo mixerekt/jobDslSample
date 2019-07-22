daf41fca1c62f14e8b004a74b50ee75c250fc906
mixerekt <ant1058@yandex.ru>
2019-07-22 18:23:37 +0500

    simple job

--- src/jobs/OneJob.groovy
+++ src/jobs/OneJob.groovy
@@ -1,1 +1,12 @@
+def repo = 'mixerekt/jobDslSample'
+def baseFolder = 'OneExample'
 
+folder(baseFolder) {
+  discription = 'Example job'
+}
+
+job() {
+  steps {
+    echo "hello form $repo"
+  }
+}
